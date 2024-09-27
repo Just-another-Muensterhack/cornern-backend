de0015dd51a6884860ef8c0d44befb3dfa4795c8
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-09-27 14:12:00 +0000

    add corner api

--- cornern/api/schema.py
+++ cornern/api/schema.py
@@ -10,3 +10,15 @@ class CornersOut(Schema):
     @staticmethod
     def resolve_position(obj):
         return (obj.lat, obj.lon)
+
+
+class CornerOut(Schema):
+    id: int
+    name: str
+    position: tuple[float, float]
+    noiseValue: float = Field(..., alias="score")
+    description: str
+
+    @staticmethod
+    def resolve_position(obj):
+        return (obj.lat, obj.lon)
--- cornern/api/views.py
+++ cornern/api/views.py
@@ -1,11 +1,16 @@
 from ninja import NinjaAPI
 
 from .models import Corner
-from .schema import CornersOut
+from .schema import CornerOut, CornersOut
 
 api = NinjaAPI()
 
 
 @api.get("/corner", response=list[CornersOut])
-def hello(request):
+def corners(request):
     return list(Corner.objects.all())
+
+
+@api.get("/corner/{corner_id}", response=CornerOut)
+def corner(request, corner_id: int):
+    return Corner.objects.get(id=corner_id)
