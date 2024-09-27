043b6426009fa90282af5bc799e256e88d946183
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-09-27 19:31:48 +0000

    add cors

--- cornern/cornern/settings.py
+++ cornern/cornern/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = ["*"]
