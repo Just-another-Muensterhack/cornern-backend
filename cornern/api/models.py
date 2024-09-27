b915bbdbba30086572249b336cd31dfc44407d24
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-09-27 13:39:30 +0000

    naming

--- cornern/api/models.py
+++ cornern/api/models.py
@@ -1,15 +1,21 @@
 import base64
+from datetime import datetime, timedelta
 from uuid import uuid4
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class Corner(models.Model):  # score
+class Corner(models.Model):
+    class Meta:
+        verbose_name = _("Corner")
+        verbose_name_plural = _("Corners")
+
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    lat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    lon = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    description = models.TextField(verbose_name="Beschreibung", null=True, blank=True)
+    lat = models.DecimalField(verbose_name="Breitengrat", max_digits=9, decimal_places=6, null=True)
+    lon = models.DecimalField(verbose_name="Längengrat", max_digits=9, decimal_places=6, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -18,10 +24,20 @@ class Corner(models.Model):  # score
 
     @property
     def score(self):
-        return 0
+        return (
+            Measurement.objects.filter(
+                sensor__corner=self,
+                created_at__gte=datetime.now() - timedelta(minutes=30),
+            ).aggregate(models.Avg("value"))["value__avg"]
+            or 0
+        )
 
 
-class Sensor(models.Model):  # Secret
+class Sensor(models.Model):
+    class Meta:
+        verbose_name = _("Sensor")
+        verbose_name_plural = _("Sensoren")
+
     name = models.CharField(max_length=100)
     corner = models.ForeignKey(Corner, on_delete=models.CASCADE)
     token = models.UUIDField(default=uuid4)
@@ -36,8 +52,12 @@ class Sensor(models.Model):  # Secret
 
 
 class Measurement(models.Model):
-    value = models.IntegerField()
-    prediction = models.FloatField()
+    class Meta:
+        verbose_name = _("Messung")
+        verbose_name_plural = _("Messungen")
+
+    value = models.IntegerField(verbose_name="Wert")
+    prediction = models.FloatField(verbose_name="Vorhersage")
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -46,17 +66,21 @@ class Measurement(models.Model):
 
 
 class Feedback(models.Model):
-    class LoudnessType(models.TextChoices):
-        VERYLOUD = "Sehr laut"
-        LOUD = "Laut"
-        NEUTRAL = "neutral"
-        QUIET = "leise"
-        VERYQUIET = "Sehr leise"
+    class Meta:
+        verbose_name = _("Feedback")
+        verbose_name_plural = _("Feedbacks")
 
-    subject = models.CharField(max_length=100)
-    message = models.TextField()
+    class LoudnessType(models.TextChoices):
+        VERYLOUD = "Sehr laut", _("Sehr laut")
+        LOUD = "Laut", _("Laut")
+        NEUTRAL = "neutral", _("Neutral")
+        QUIET = "leise", _("Leise")
+        VERYQUIET = "Sehr leise", _("Sehr leise")
+
+    subject = models.CharField(verbose_name="Thema", max_length=100)
+    message = models.TextField(verbose_name="Nachricht")
     corner = models.ForeignKey(Corner, on_delete=models.CASCADE)
-    loudness = models.CharField(choices=LoudnessType)
+    loudness = models.CharField(verbose_name="Lautstärke", choices=LoudnessType)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
