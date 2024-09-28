efde8296b11272bc842d9964281ddb7ffb00416b
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-09-28 08:28:38 +0000

    fix csrf

--- cornern/cornern/settings.py
+++ cornern/cornern/settings.py
@@ -132,4 +132,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*", "http://*"]
